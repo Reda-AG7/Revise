692372827596200148754f233528ba16f9c47561
Reda Agourram <ragourram.7@gmail.com>
2024-09-18 10:39:41 -0400

    adding the divide function

--- calc.cpp
+++ calc.cpp
@@ -9,9 +9,20 @@ template<typename T>
 T add(T a, T b) requires Numeric<T>{
 	return a + b;
 }
+template<typename T>
+T devide(T a, T b) requires Numeric<T>{
+	if (b == 0) throw std::format("b cannot be zero");
+	return a / b;
+}
 int main() {
 	int a = 10;
-	int b = 15;
+	int b = 0;
 	std::cout<<std::format("{} + {} = {}\n", a, b, add(a,b));
-
+	try{
+		int x = devide(a,b);
+		std::cout<<std::format("{} / {} = {}\n", a, b, x); 
+	}
+	catch(std::string error){
+		std::cout<<error<<std::endl;
+	}
 }
